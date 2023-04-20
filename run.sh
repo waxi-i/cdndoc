c109cbbfa174517054235baa7e7c5ed079b78c0a
Your Name <you@example.com>
2023-04-21 01:23:15 +0800

    123

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ cd /apps/v2doc
 
 gitbook build ./dev ./docs
 
-find ./docs -type f -name "*.html" | xargs sed -i 's#https://www.gitbook.com##g'
-find ./docs -type f -name "*.html" | xargs sed -i 's#本书使用 GitBook 发布#End#g'
+# find ./docs -type f -name "*.html" | xargs sed -i 's#https://www.gitbook.com##g'
+# find ./docs -type f -name "*.html" | xargs sed -i 's#本书使用 GitBook 发布#End#g'
 
 # 插件 http://gitbook.zhangjikai.com/plugins.html
