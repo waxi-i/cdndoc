9446559cc7bca02f55739ac1f7d9799a5ed37b68
Your Name <you@example.com>
2023-04-24 00:54:29 +0800

    123

--- run.sh
+++ run.sh
@@ -6,6 +6,9 @@ cd /apps/v2doc
 
 gitbook build ./prod ./docs
 
+git add .
+git commit -m "$(date +%FT%T)"
+git push 
 # find ./docs -type f -name "*.html" | xargs sed -i 's#https://www.gitbook.com##g'
 # find ./docs -type f -name "*.html" | xargs sed -i 's#本书使用 GitBook 发布#End#g'
 
